7030d10aceaac1f1586f2b9541c7659f84ba4663
hlion17 <yourdad20626@gmail.com>
2022-04-28 12:18:36 +0900

    initail with spring boot starter

---
+++ src/test/java/me/project/funding/FundingApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.funding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
