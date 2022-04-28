8f4df9c3385ae29ffa942ce9a12e6f5688ba91ba
hlion17 <yourdad20626@gmail.com>
2022-04-28 16:19:42 +0900

    view 관련 설정, 내장톰켓 jsp 라이브러리 추가, internalResouceViewResolver 설정 추가

---
+++ src/main/java/me/project/funding/config/appConfig.java
@@ -0,0 +1,20 @@
+//package me.project.funding.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+//@Configuration
+//@EnableWebMvc
+//public class appConfig {
+//
+//    @Bean
+//    public ViewResolver customViewResolver() {
+//        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+//        internalResourceViewResolver.setPrefix("/WEB-INF/");
+//        internalResourceViewResolver.setSuffix(".jsp");
+//        return internalResourceViewResolver;
+//    }
+//}
--- src/main/java/me/project/funding/controller/MemberController.java
+++ src/main/java/me/project/funding/controller/MemberController.java
@@ -3,14 +3,16 @@ package me.project.funding.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
 public class MemberController {
 
     @GetMapping("/test")
-    public String test() {
+    public ModelAndView test() {
+        ModelAndView mav = new ModelAndView("test/go");
         log.info("[/test][GET]");
-        return "test/go";
+        return mav;
     }
 }
