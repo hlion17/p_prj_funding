5c9fc6fd1cb231f48e37375f55937ce3ec130bca
hlion17 <yourdad20626@gmail.com>
2022-04-28 22:04:00 +0900

    JSON view Resolver 빈 등록 설정

--- src/main/java/me/project/funding/config/WebConfig.java
+++ src/main/java/me/project/funding/config/WebConfig.java
@@ -1,8 +1,10 @@
 package me.project.funding.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,4 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
 //                .addResourceLocations("classpath:/webapp/resource")
 //                .setCachePeriod(20);
 //    }
+
+    // JSON View Resolver 빈 등록
+    @Bean
+    public MappingJackson2JsonView jsonView() {
+        return new MappingJackson2JsonView();
+    }
 }
